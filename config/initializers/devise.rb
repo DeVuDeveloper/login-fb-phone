1756094f023f0e6ddc99c43aecf7478be85f27a2
DeVuDeveloper <dejanvu.developer@gmail.com>
2023-05-12 11:10:03 +0200

    Linter for Ruby and Tailwind CSS and GitHub actions for it and RSpec

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # config.omniauth :facebook, Rails.application.credentials.facebook[:APP_ID],
-  #   Rails.application.credentials.facebook[:APP_SECRET], token_params: {parse: :json}
+  #    Rails.application.credentials.facebook[:APP_SECRET], token_params: {parse: :json}
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
